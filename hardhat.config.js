b6bc868c920b340d5b44b272799a27c4243fa84d
karangorania <karangoraniya@gmail.com>
2022-07-06 18:05:49 +0530

    gas reporter config added 🚀

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,59 +1,40 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-etherscan');
+require('hardhat-gas-reporter');
 require('dotenv').config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+const { POLYGON_API_URL, PRIVATE_KEY, POLYGON_SCAN_KEY } = process.env;
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
+  gasReporter: {
+    currency: 'ETH',
+    gasPrice: 21,
+  },
   solidity: {
-    version: "0.8.4",
+    version: '0.8.4',
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
+        runs: 200,
+      },
+    },
   },
   networks: {
     hardhat: {
       gas: 12000000,
+      gasPrice: 8000000000,
       blockGasLimit: 0x1fffffffffffff,
       allowUnlimitedContractSize: true,
-      timeout: 1800000
+      timeout: 1800000,
     },
-    rinkeby: {
-      gas: 12000000,
-      url: process.env.RINKEBY_HTTP_INFURA || '',
+    mumbai: {
+      url: POLYGON_API_URL,
       accounts: {
-        mnemonic: process.env.MNEMONICS,
+        mnemonic: PRIVATE_KEY,
       },
-      chainId: 4,
-      minGasPrice: 42000000000000,
-    },
-    ropsten: {
-      gas: 12000000,
-      minGasPrice: 42000000000000,
-      url: process.env.ROPSTEN_HTTP_INFURA || '',
-      accounts: {
-        mnemonic: process.env.MNEMONICS,
-      },
-      chainId: 3,
     },
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  }
+    apiKey: POLYGON_SCAN_KEY,
+  },
 };
